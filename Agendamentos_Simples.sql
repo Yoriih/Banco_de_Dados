7e0c643ae1a076e153bc8e70f6d63295da6ddf07
Nicolas Gonçalves <61568561+Yoriih@users.noreply.github.com>
2025-06-27 19:54:16 -0300

    Update Agendamentos_Simples.sql

--- Agendamentos_Simples.sql
+++ Agendamentos_Simples.sql
@@ -1,5 +1,5 @@
 CREATE TABLE agendamentos (
-	id_agendamentos INT PRIMARY KEY auto_increment,
+    id_agendamentos INT PRIMARY KEY auto_increment,
     cpf_cliente VARCHAR (14),
     servico VARCHAR (100),
     data_agendada DATE,
@@ -7,7 +7,7 @@ CREATE TABLE agendamentos (
     observacoes TEXT 
 );
 INSERT INTO agendamentos (cpf_cliente, servico, data_agendada, hora_agendada, observacoes) VALUES 
-	('366.408.050-57', 'Oftalmologista', '2025-06-28', '10:30:00', 'Cliente PCD'),
+    ('366.408.050-57', 'Oftalmologista', '2025-06-28', '10:30:00', 'Cliente PCD'),
     ('406.635.710-57', 'Clinico Geral', '2025-07-01', '12:40:00', 'Coletar Exames'),
     ('195.560.990-00', 'Ginecologista', '2025-07-05', '14:15:00', 'Atendimento Somente com Acompanhante'),
     ('833.042.280-12', 'Quiropraxia', '2025-07-10', '16:00:00', 'Primeira Consulta'),
